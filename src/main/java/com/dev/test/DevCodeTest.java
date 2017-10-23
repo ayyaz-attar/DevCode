664eed29aaedb343064da30943a097a64266bd99
Ayyaz <ayyaz.attar@kickdrumtech.com>
2017-10-23 13:02:20 +0530

    changes sample 1

--- src/main/java/com/dev/test/DevCodeTest.java
+++ src/main/java/com/dev/test/DevCodeTest.java
@@ -5,6 +5,7 @@ public class DevCodeTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Dev code Deployed");
+		System.out.println("Dev code Deployed");
 	}
 
 }
